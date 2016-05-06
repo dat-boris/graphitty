dfd0adb250b1cf35b78f7fad05646e42758e25d0
Boris Lau <boris@mobify.com>
2016-05-06 12:42:02 +0100

    Restructure running script

--- graphitty_csv.py
+++ graphitty_csv.py
@@ -2,15 +2,19 @@
 
 """
 Script for consuming given file into output image
+
+graphitty_csv csv1 csv1 combine_output.png
 """
 import sys
 
 import pandas as pd
 from graphitty.graphitty import Graphitty
+from graphitty.combiner import GraphCombiner
 from nxpd import draw
 
 
-def run_simplication(csv, output_png):
+def run_simplication(csv):
+    output_png = csv + '.png'
     df = pd.read_csv(csv)
     g = Graphitty(
         df,
@@ -23,17 +27,31 @@ def run_simplication(csv, output_png):
         min_edges=0,
         filter_subgraph=False
     )
-    draw(nx_orig, "original_" + output_png, show=False)
+    draw_with_output(nx_orig, "original_" + output_png)
 
     nx_orig = g.create_graph(
         min_edges=0,
         filter_subgraph=True
     )
-    draw(nx_orig, "simplified_" + output_png, show=False)
+    draw_with_output(nx_orig, "simplified_" + output_png)
 
     nx_condense = g.simplify()
-    draw(nx_condense, output_png, show=False)
+    draw_with_output(nx_condense, output_png)
 
+    return g
+
+
+def draw_with_output(g, f):
+    print "Drawing: {}".format(f)
+    draw(g, f, show=False)
 
 if __name__ == '__main__':
-    run_simplication(sys.argv[1], sys.argv[2])
+    csv1 = sys.argv[1]
+    csv2 = sys.argv[2]
+    imgout = sys.argv[3]
+    g1 = run_simplication(csv1)
+    g2 = run_simplication(csv2)
+
+    g = GraphCombiner(g1, g2)
+    nx_combined = g.create_graph()
+    draw_with_output(nx_combined, imgout)
