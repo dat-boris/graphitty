8636feba33cee516256289678ec9e6570216a695
Boris Lau <boris@khanacademy.org>
2019-01-22 09:49:31 -0800

    Added code for rendering dropoff

--- graphitty/funnel.py
+++ graphitty/funnel.py
@@ -1,4 +1,6 @@
+import random
 import pandas as pd
+import seaborn as sns
 from collections import defaultdict
 
 
@@ -7,13 +9,19 @@ class Funnel(object):
     Simulate the funnel of each step across the graph
     """
 
-    def __init__(self, path, df, graph):
+    def __init__(self, path, graph):
         self.funnel_path = path
         self.graph = graph
         self.user_max_steps = {}
 
+        # tranfer parameters from graph
+        self.df = graph.df
+        self.id_col = graph.id_col
+        self.behaviour_col = graph.behaviour_col
+        self.ts_col = graph.ts_col
+
         self.steps = self.identify_user_steps(path, graph)
-        # self.mark_df_funnel_steps()
+        self.annotated_df = None
 
     def is_common_path(self, user_path, funnel_path=None):
         """ Identify the maximum path that user have in common
@@ -61,6 +69,95 @@ class Funnel(object):
         del self.user_max_steps[user_id]
         for s in self.steps[:max_steps]:
             s['user'].remove(user_id)
+
+    def filter_df_by_users(self, df=None, id_col=None, sample=None):
+        if df is None:
+            df = self.df
+        if id_col is None:
+            id_col = self.id_col
+        user_in_funnel = self.user_max_steps.keys()
+        if sample:
+            user_in_funnel = random.sample(user_in_funnel, k=sample)
+        return df[df[id_col].isin(user_in_funnel)]
+
+    @property
+    def funnel_path_in_df(self):
+        # These are nodes which is added manually
+        ignore_nodes = {'start', 'end'}
+        funnel_path = [
+            p for p in self.funnel_path if p not in ignore_nodes
+        ]
+        return funnel_path
+
+    def __label_group(self, group, include_intermediate=False):
+        """
+        Internal function for parsing path.
+        Identify steps that are in those steps and then return those.
+
+        Used in pandas.apply
+        Adding two columns:
+        * last_step (yes / no)
+        * step_count
+        """
+        funnel_path = self.funnel_path_in_df
+
+        path_idx = 0
+        sorted_time = group.sort_values(self.ts_col)
+        wanted_steps = []
+        for _, row in sorted_time.iterrows():
+            # Identify what step the user is at
+            path_name = funnel_path[path_idx]
+            current_name = row[self.behaviour_col]
+            if path_name == current_name:
+                path_idx += 1
+                # check if user is at last step:
+                user_id = row[self.id_col]
+                max_step = self.user_max_steps[user_id]
+                row['step_count'] = path_idx
+                wanted_steps.append(row)
+            else:
+                if include_intermediate:
+                    row['step_count'] = path_idx
+                    wanted_steps.append(row)
+
+        if not wanted_steps:
+            return None
+        return_df = pd.DataFrame(wanted_steps)
+        return_df['is_last_step'] = (return_df['step_count'] == path_idx)
+        return return_df
+
+    def extract_key_steps_from_df(self, df=None, id_col=None, sample=None):
+        """ Given a trail dataframe, extract the key steps
+        """
+        if df is None:
+            df = self.df
+        if id_col is None:
+            id_col = self.id_col
+
+        filter_df = self.filter_df_by_users(df, sample=sample)
+
+        annotated_df = pd.DataFrame(
+            filter_df.groupby(id_col).apply(
+                self.__label_group
+            )
+        ).reset_index(drop=True)
+        self.annotated_df = annotated_df
+        return annotated_df
+
+    def visualize_dropoff(self, compare_col='sufficiently_usable'):
+        annotated_df = self.annotated_df
+        funnel_path = self.funnel_path_in_df
+        # it doesnt make sense for last step
+        for i, step_name in enumerate(funnel_path[:-1]):
+            step_df = annotated_df[annotated_df.step_count == i+1]
+            sns.jointplot(
+                step_df['is_last_step'],
+                step_df[compare_col],
+                kind='reg'
+            ).fig.suptitle(
+                'Step {}: comparing {} to dropoff ratio'.format(
+                    step_name, compare_col
+                ))
 
 
 class FunnelCombination(object):
